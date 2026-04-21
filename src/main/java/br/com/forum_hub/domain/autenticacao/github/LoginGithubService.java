6eec6e6ec166f7028697d203164d7f46f05ae5b1
Eduardo Borges <eduardo_borges98@hotmail.com>
2026-04-21 15:42:28 -0300

    token github

---
+++ src/main/java/br/com/forum_hub/domain/autenticacao/github/LoginGithubService.java
@@ -0,0 +1,41 @@
+package br.com.forum_hub.domain.autenticacao.github;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@Service
+public class LoginGithubService {
+
+    private final String clientId = "Ov23liOYXMzHiT3gI6lG";
+    private final String clientSecret = "1561fc026f1adc90f981bd5e0f226a379060577c";
+    private final String redirectUri = "http://localhost:8080/login/github/autorizado";
+    private final RestClient restClient;
+
+    public LoginGithubService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+
+    public String gerarUrl(){
+        return "https://github.com/login/oauth/authorize"+
+                "?client_id="+clientId +
+                "&redirect_uri="+redirectUri +
+                "&scope=read:user,user:email";
+    }
+
+
+    public String obterToken(String code) {
+        var resposta = restClient.post()
+                .uri("https://github.com/login/oauth/access_token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Map.of("code", code, "client_id", clientId,
+                        "client_secret", clientSecret, "redirect_uri", redirectUri))
+                .retrieve()
+                .body(String.class);
+        return resposta;
+    }
+}
